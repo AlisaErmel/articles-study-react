337e8c1cc175f714ff95036c307332b4085cce7d
Alisa Ermel <alisa.ermel@gmail.com>
2026-02-03 13:58:13 +0200

    chore: add axe-core accessibility checks during development

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+// Accessibility checks only during the development
+if (import.meta.env.MODE === 'development') {
+  import('@axe-core/react').then((axe) => {
+    axe.default(React, undefined, 1000)
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
